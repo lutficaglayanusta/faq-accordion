0e5c075ddc1838be974b18a8a3e201f82bf0c9e3
caglayan <caglayanustaa@gmail.com>
2025-11-21 22:34:57 +0300

    javascript done

---
+++ index.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+const description = document.querySelectorAll(".description");
+
+container.addEventListener("click", (e) => {
+  if (e.target.nodeName === "IMG") {
+    console.log(e.target.classList);
+    if (e.target.classList.contains("minus-image")) {
+      e.target.style.display = "none";
+      e.target.nextElementSibling.style.display = "block";
+      e.target.parentElement.parentElement.nextElementSibling.style.display ="none";
+    } else {
+      e.target.style.display = "none";
+      e.target.previousElementSibling.style.display = "block";
+      e.target.parentElement.parentElement.nextElementSibling.style.display ="block";
+    }
+  }
+});
